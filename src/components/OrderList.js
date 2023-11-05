566246e32f0ad715c498ce219101f48446b0be54
Ubuntu <ubuntu@ip-172-31-37-41.us-east-2.compute.internal>
2023-11-05 17:21:21 +0000

    Add .gitignore and remove node_modules

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import mockOrders from './mockOrders'; // Make sure this path is correct
+import React, { useState, useEffect } from 'react';
+import mockOrders from './mockOrders';
 
-const OrderList = ({ statusFilter }) => {
-  // Directly use mockOrders as the state
-  const [orders] = React.useState(mockOrders);
+const OrderList = () => {
+  const [orders, setOrders] = useState(mockOrders);
+  const [statusFilter, setStatusFilter] = useState('all');
 
-  // Filter orders based on the statusFilter prop
+  useEffect(() => {
+    // This effect runs when the statusFilter changes.
+    // If you are fetching orders from a server, you would trigger that here.
+  }, [statusFilter]);
+
+  const handleStatusChange = (orderNumber, newStatus) => {
+    const updatedOrders = orders.map(order => {
+      if (order.orderNumber === orderNumber) {
+        return { ...order, status: newStatus };
+      }
+      return order;
+    });
+    setOrders(updatedOrders);
+    // Here you would also update the server with the new status
+  };
+
+  const handleFilterChange = (newFilter) => {
+    setStatusFilter(newFilter);
+  };
+
   const filteredOrders = statusFilter === 'all' ? orders : orders.filter(order => order.status === statusFilter);
 
   return (
     <div>
       <h2>Order List</h2>
+      <div>
+        <button onClick={() => handleFilterChange('all')}>All</button>
+        <button onClick={() => handleFilterChange('pending')}>Pending</button>
+        <button onClick={() => handleFilterChange('in progress')}>In Progress</button>
+        <button onClick={() => handleFilterChange('ready for pickup')}>Ready for Pickup</button>
+        <button onClick={() => handleFilterChange('completed')}>Completed</button>
+      </div>
       <ul>
         {filteredOrders.map((order, index) => (
-          // Add className to li element
-          <li key={index} className={`order-item status-${order.status.replace(/\s+/g, '-').toLowerCase()}`}>
-            Order #{order.orderNumber}: {order.customerName} - Total: ${order.totalPrice} - Status: {order.status}
-            <ul>
-              {order.items.map((item, itemIndex) => (
-                <li key={itemIndex}>{item}</li>
-              ))}
-            </ul>
+          <li key={index}>
+            Order #{order.orderNumber}: {order.customerName} - Total: ${order.totalPrice} - Status:
+            <select value={order.status} onChange={(e) => handleStatusChange(order.orderNumber, e.target.value)}>
+              <option value="pending">Pending</option>
+              <option value="in progress">In Progress</option>
+              <option value="ready for pickup">Ready for Pickup</option>
+              <option value="completed">Completed</option>
+            </select>
           </li>
         ))}
       </ul>
--- src/components/OrderManagement.js
+++ src/components/OrderManagement.js
@@ -1,24 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import OrderList from './OrderList';
 
 const OrderManagement = () => {
-  const [statusFilter, setStatusFilter] = useState('all');
-
-  const handleFilterChange = (status) => {
-    setStatusFilter(status);
-  };
-
   return (
     <div>
       <h1>Order Management</h1>
-      <div>
-        <button onClick={() => handleFilterChange('all')}>All</button>
-        <button onClick={() => handleFilterChange('pending')}>Pending</button>
-        <button onClick={() => handleFilterChange('in progress')}>In Progress</button>
-        <button onClick={() => handleFilterChange('ready for pickup')}>Ready for Pickup</button>
-        <button onClick={() => handleFilterChange('completed')}>Completed</button>
-      </div>
-      <OrderList statusFilter={statusFilter} />
+      <OrderList />
     </div>
   );
 };
